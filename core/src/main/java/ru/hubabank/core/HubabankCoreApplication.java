e390460dc6472e45e519167a451345dc4f6e70c1
Роман Аникушин <r.anikushin@cft.ru>
2024-03-08 22:18:47 +0700

    Update .gitlab-ci.yml file

--- core/src/main/java/ru/hubabank/core/HubabankCoreApplication.java
+++ core/src/main/java/ru/hubabank/core/HubabankCoreApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class HubabankCoreApplication {
 
     public static void main(String[] args) {
-        // g
         SpringApplication.run(HubabankCoreApplication.class, args);
     }
 
--- java/users/src/main/java/org/huba/users/UsersApplication.java
+++ java/users/src/main/java/org/huba/users/UsersApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsersApplication {
 
 	public static void main(String[] args) {
-		// g
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
