d8b83c61028385f747226ba3525c271a749576d7
Роман Аникушин <r.anikushin@cft.ru>
2024-03-08 22:04:47 +0700

    Update .gitlab-ci.yml file

--- java/users/src/main/java/org/huba/users/UsersApplication.java
+++ java/users/src/main/java/org/huba/users/UsersApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsersApplication {
 
 	public static void main(String[] args) {
-		// HIaa
+		// H
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
