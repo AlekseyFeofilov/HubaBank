a899a03ca7a439d60a9b245b220ac6a83b5a0bf6
Роман Аникушин <r.anikushin@cft.ru>
2024-03-10 00:07:32 +0700

    Добавлен момент времени в TransactionDto

--- core/src/test/resources/sql/insert-transactions.sql
+++ core/src/test/resources/sql/insert-transactions.sql
@@ -1,3 +1,3 @@
 insert into transaction(id, bill_id, balance_change, reason, instant)
-values ('eef2216f-7437-460e-9044-eeb55b0be663', '11f95aaf-1e75-43be-94c3-8f3fb283aaae', 10, 'TERMINAL', '2016-06-22 19:10:25'),
-       ('a9021df3-11aa-4182-9463-eda5a71ff73c', '11f95aaf-1e75-43be-94c3-8f3fb283aaae', -5, 'LOAN', '2022-09-01 13:54:02');
+values ('eef2216f-7437-460e-9044-eeb55b0be663', '11f95aaf-1e75-43be-94c3-8f3fb283aaae', 10, 'TERMINAL', '2016-06-22 19:10:25.621437 +00:00'),
+       ('a9021df3-11aa-4182-9463-eda5a71ff73c', '11f95aaf-1e75-43be-94c3-8f3fb283aaae', -5, 'LOAN', '2022-09-01 13:54:02.521 +00:00');
