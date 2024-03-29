c3c6d6c67bdd2a091763d819d1f04df0da648d04
Denis Builuk <den52022@gmail.com>
2024-03-29 19:03:57 +0000

    Revert "Merge branch 'revert-7955ff50' into 'master'"

---
+++ BFF-client/BFF-client.Api/Models/user/ProfileWithPrivileges.cs
@@ -0,0 +1,15 @@
+﻿using BFF_client.Api.model.user;
+
+namespace BFF_client.Api.Models.user
+{
+    public class ProfileWithPrivileges
+    {
+        public string Id { get; set; }
+
+        public FullNameDto FullNameDto { get; set; }
+
+        public string Phone { get; set; }
+
+        public List<string> Privileges {  get; set; }
+    }
+}
