c57e02b86dcc7e966beb1f0a182678c08328381c
a.feofilov <a.feofilov@tinkoff.ru>
2024-03-30 21:47:15 +0700

    add Core provider

---
+++ credit/Core.Provider/Bootstrapper.cs
@@ -0,0 +1,23 @@
+using Core.Provider.Configuration;
+using Core.Provider.v1;
+using Core.Provider.v2;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Provider;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection AddCoreProvider(this IServiceCollection services)
+    {
+        services.AddTransient<ClientConfiguration>()
+            .AddHttpClient("Core", client =>
+            {
+                client.BaseAddress = new Uri("http://194.147.90.192:9004");
+                client.DefaultRequestHeaders.Add("X-API-KEY", "55wSKcrRx4C0paIf4hLbNqhzygviC8G3igpg5kdZs5WqipmNDkeOZ149vGS1DJ9W");
+            })
+            .AddTypedClient<ICoreProviderV1, CoreProviderV1>()
+            .AddTypedClient<ICoreProviderV2, CoreProviderV2>();
+
+        return services;
+    }
+}
