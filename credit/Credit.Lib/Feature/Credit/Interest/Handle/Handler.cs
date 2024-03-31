02e5bc9680deb0d920f945c4a98f87f352f58d23
a.feofilov <a.feofilov@tinkoff.ru>
2024-03-31 23:59:34 +0700

    fix

--- credit/Credit.Lib/Feature/Credit/Interest/Handle/Handler.cs
+++ credit/Credit.Lib/Feature/Credit/Interest/Handle/Handler.cs
@@ -20,6 +20,8 @@ public class Handler : IRequestHandler<Request>
         var interest = credit.Principal * credit.InterestRate / (DateTime.IsLeapYear(DateTime.Today.Year) ? 366 : 365);
         var paymentAmount = Math.Min((long)interest, billBalance);
         
+        if (paymentAmount == 0) return;
+        
         await _mediator.Send(new MasterBill.Deposit.Request(credit.BillId, paymentAmount), cancellationToken);
         await _mediator.Send(new Credit.Update.Request(credit.Id, new Data.Requests.Credit.UpdateRequest
         {
